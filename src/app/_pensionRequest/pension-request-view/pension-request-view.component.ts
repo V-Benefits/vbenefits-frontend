722805c389d3789ee7fb3c7d7e7308b0752a2d9d
MonaFawzy92 <58727333+MonaFawzy92@users.noreply.github.com>
2020-04-28 22:33:45 +0200

    snack bar shared component

--- src/app/_pensionRequest/pension-request-view/pension-request-view.component.ts
+++ src/app/_pensionRequest/pension-request-view/pension-request-view.component.ts
@@ -1,21 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { publicService } from 'src/app/core/publicService.service';
 import { RoundModel } from '../pensionRequestModels';
-import { MatDialog, MatDialogConfig } from '@angular/material';
+import { MatDialog, MatDialogConfig, MatSnackBar } from '@angular/material';
 import { PensionRequestModalComponent } from '../pension-request-modal/pension-request-modal.component';
 import {formatDate} from '@angular/common'
+import { SnackBarComponent } from 'src/app/shared/snack-bar/snack-bar.component';
 @Component({
   selector: 'app-pension-request-view',
   templateUrl: './pension-request-view.component.html',
-  styleUrls: ['./pension-request-view.component.css']
+  styleUrls: ['./pension-request-view.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
+
 export class PensionRequestViewComponent implements OnInit {
   rounds:RoundModel[];
  
   disableBtn:boolean = false;
-
-  currentDate :string ;
-  currentDate2:Date = new Date();
+  currentDate:Date = new Date();
 
   firstRoundStartDate:Date = new Date("2020-02-18T00:00:00");
   firstRoundEndDate:Date = new Date("2020-02-25T00:00:00");
@@ -23,12 +24,12 @@ export class PensionRequestViewComponent implements OnInit {
   secondRoundStartDate:Date =new Date("2020-11-18T00:00:00");
   secondRoundEndDate:Date = new Date("2020-11-25T00:00:00");
    
-  constructor( private service: publicService,public dialog: MatDialog) { }
+  constructor( private service: publicService,
+               public dialog: MatDialog,
+               private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.getAllRounds();
-    
-    this.currentDate =  formatDate(new Date(), 'yyyy-MM-dd', 'en');
     this.compareDates();
   }
 
@@ -47,17 +48,23 @@ export class PensionRequestViewComponent implements OnInit {
   }
 
   compareDates() {
-    debugger
-    if ((this.firstRoundStartDate <= this.currentDate2 && this.currentDate2 <= this.firstRoundEndDate) ||
-     (this.secondRoundStartDate <= this.currentDate2 && this.currentDate2 <= this.secondRoundEndDate )) {
+    if ((this.firstRoundStartDate <= this.currentDate && this.currentDate <= this.firstRoundEndDate) ||
+      (this.secondRoundStartDate <= this.currentDate && this.currentDate <= this.secondRoundEndDate)) {
       this.disableBtn = false;
-      console.log("trueeeeeeeeeeeeeeeee");
     }
     else {
-      this.disableBtn = true;
-      console.log("falseeeeeeeee");
+      this.disableBtn = true;    
+      this.openSnackBar();
     }
+  }
 
+  openSnackBar(){
+    this._snackBar.openFromComponent(SnackBarComponent, {
+      data: 'Not available! Please check pension round dates .',
+      panelClass: ['snackbar'],
+      verticalPosition: 'bottom',
+      horizontalPosition: 'end',
+    });
   }
   }
 
